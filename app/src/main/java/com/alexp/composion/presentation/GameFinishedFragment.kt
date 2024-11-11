eda947b87db9fee2c8d7131b89227c6633f3af03
Alex <a.u.porfiref@niissu.ru>
2024-11-11 16:23:40 +0300

    add fragments

---
+++ app/src/main/java/com/alexp/composion/presentation/GameFinishedFragment.kt
@@ -0,0 +1,33 @@
+package com.alexp.composion.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.alexp.composion.R
+import com.alexp.composion.databinding.FragmentGameBinding
+
+class GameFinishedFragment : Fragment() {
+
+    private var _binding:FragmentGameBinding? = null
+    private val binding : FragmentGameBinding
+        get() = _binding?:throw RuntimeException("FragmentGameBinding is null")
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding =null
+    }
+}
